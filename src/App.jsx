88a6ee40f67a3049bb1d654280cdc5b5ee2e21c8
Aymannagyahmed <aymannagy.ahmed@gmail.com>
2023-09-26 03:37:01 +0300

    add tailwindcss, @reduxjs/toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-   <>
-      App
-   </>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
